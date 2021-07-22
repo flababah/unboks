cf338c270027894d04c5bab805be9ee8ae4960dc
Anders Høst <anders@foob.dk>
2021-07-22 18:25:13 +0200

    Add docs and remove outdated comments.

--- unboks/src/main/kotlin/unboks/internal/DependencySpecs.kt
+++ unboks/src/main/kotlin/unboks/internal/DependencySpecs.kt
@@ -2,8 +2,17 @@ package unboks.internal
 
 import unboks.*
 
+/**
+ * Specification of a relationship between object types. Ie. some sort of collection from A to its
+ * associated B types. The B types contain reference counts of the As pointing to them.
+ *
+ * @param accessor returns the [RefCount] instance used to track referencing objects
+ */
 internal class TargetSpecification<A, B>(val accessor: (B) -> RefCount<A>)
 
+// +---------------------------------------------------------------------------
+// |  Publicly used specifications below
+// +---------------------------------------------------------------------------
 
 internal val blockInputs = TargetSpecification<Block, BasicBlock> { it.predecessors }
 
@@ -12,26 +21,3 @@ internal val handlerUses = TargetSpecification<Block, HandlerBlock> { it.predece
 internal val phiReferences = TargetSpecification<IrPhi, Block> { it.phiReferences }
 
 internal val defUses = TargetSpecification<Use, Def> { it.uses }
-
-// TODO constant values. (IrConstant)
-
-// TODO class hierarchy. and shit...
-
-// TODO Kan vi lave klasser?
-
-/*
-BB
-- inputs
-- phi references
-
-Def
-- uses
-
-
------- New
-
-exception table Pair<HandlerBlock, Reference?>
-
-BB usage i Irs.
-
- */
